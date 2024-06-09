6ab3221a2800ce3b4bde17cd7e5cf7e951181232
ishi12121 <isht1402@gmail.com>
2024-06-09 16:42:51 +0530

    basic setup done

--- finance-app/app/(dashboard)/page.tsx
+++ finance-app/app/(dashboard)/page.tsx
@@ -2,8 +2,6 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <>
-      <UserButton afterSignOutUrl="/" />
-    </>
+ <div>Dashboard</div>
   );
 }
---
+++ finance-app/app/api/[[...route]]/route.ts
@@ -0,0 +1,21 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+const app = new Hono().basePath("/api");
+
+app.get("/hello", clerkMiddleware(), (c) => {
+  const auth = getAuth(c);
+  if (!auth?.userId) {
+    return c.json({
+      error: "Unauthorized",
+    });
+  }
+  return c.json({
+    message: "Hello, world!",
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
